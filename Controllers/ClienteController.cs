5573163d8320b25a520431ef08b9450747d639db
Eduardo Rosales <eduardo.rosales@sodisa.online>
2023-03-20 20:11:33 -0600

    Inicio de Proyecto

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSVenta.Models;
+
+namespace WSVenta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using (VentaRealContext context = new VentaRealContext())
+            {
+                var list = context.Clientes.ToList();
+                return Ok(list);
+            }
+                
+        }
+    }
+}
---
+++ Models/VentaRealContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WSVenta.Models;
+
+public partial class VentaRealContext : DbContext
+{
+    public VentaRealContext()
+    {
+    }
+
+    public VentaRealContext(DbContextOptions<VentaRealContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Concepto> Conceptos { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Ventum> Venta { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-OBP8HTA;Initial Catalog=Venta_Real;Integrated Security=True; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Concepto>(entity =>
+        {
+            entity.ToTable("Concepto");
+
+            entity.Property(e => e.IdProducto).HasColumnName("Id_Producto");
+            entity.Property(e => e.IdVenta).HasColumnName("Id_Venta");
+            entity.Property(e => e.Importe).HasColumnType("decimal(16, 2)");
+            entity.Property(e => e.PrecioUnitario)
+                .HasColumnType("decimal(16, 2)")
+                .HasColumnName("Precio_Unitario");
+
+            entity.HasOne(d => d.IdProductoNavigation).WithMany(p => p.Conceptos)
+                .HasForeignKey(d => d.IdProducto)
+                .HasConstraintName("FK_Concepto_Producto");
+
+            entity.HasOne(d => d.IdVentaNavigation).WithMany(p => p.Conceptos)
+                .HasForeignKey(d => d.IdVenta)
+                .HasConstraintName("FK_Concepto_Venta");
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.ToTable("Producto");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Costo).HasColumnType("decimal(16, 0)");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.PrecioUnitario).HasColumnType("decimal(16, 2)");
+        });
+
+        modelBuilder.Entity<Ventum>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Fecha).HasColumnType("datetime");
+            entity.Property(e => e.IdCliente).HasColumnName("Id_Cliente");
+            entity.Property(e => e.Total).HasColumnType("decimal(16, 2)");
+
+            entity.HasOne(d => d.IdClienteNavigation).WithMany(p => p.Venta)
+                .HasForeignKey(d => d.IdCliente)
+                .HasConstraintName("FK_Venta_Clientes");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
